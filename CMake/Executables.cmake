89e2e8950b122727a8dd69828bd13a228d687117
geekymoose <constantin.masson@umontreal.ca>
2017-11-17 18:30:20 -0500

    Add working link with ElephantLogger lib and remove Singleton class

--- CMake/Executables.cmake
+++ CMake/Executables.cmake
@@ -1,5 +1,3 @@
-# The main server executable
-
 include_directories("${CMAKE_SOURCE_DIR}/include")
 
 file(GLOB_RECURSE srcFiles src/*.cpp)
@@ -9,7 +7,8 @@ add_executable(collab-server ${srcFiles})
 target_link_libraries(collab-server
     ${CJSON_LIBRARY}
     ${ELEPHANT_LIBRARY}
+    "pthread"
+    "stdc++fs"
     "-lzmq")
 
 add_custom_target(run collab-server)
-
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <logger/ElephantLogger.h>
 
 int main(int argc, char** argv) {
+    ElephantLogger::init();
+    LOG_INFO(0, "Start server");
     collab::Server server;
     server.start();
     return 0;
