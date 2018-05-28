ea5f447762ab0c3d6924c778eff6f29bd43c2d2d
geekymoose <constantin.masson@umontreal.ca>
2018-05-28 16:37:22 -0400

    Add basic Room, User and add gtest unit tests in build

--- include/collab/core/Room.h
+++ include/collab/core/Room.h
@@ -1,15 +1,29 @@
 #pragma once
 
+#include <unordered_map>
+
 namespace collab {
 
 
 /**
+ * TODO Documentation
  *
+ * \author  Constantin Masson
+ * \date    May 2018
  */
 class Room {
+    private:
+        std::unordered_map<int, User> _users;
+
     public:
         Room() = default;
         ~Room() = default;
+
+    public:
+        bool addUser(const int id);
+        bool removeUser(const int id);
+        User& getUser(const int id);
+        int getNbUsers() const;
 };
 
 
---
+++ include/collab/core/User.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace collab {
+
+
+/**
+ * TODO Documentation
+ *
+ * \author  Constantin Masson
+ * \date    May 2018
+ */
+class User {
+    private:
+        int _id;
+
+
+    public:
+        User(const int id) : _id(id) {
+        }
+};
+
+
+} // End namespace
