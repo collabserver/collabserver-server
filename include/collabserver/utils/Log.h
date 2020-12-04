40d5d1de8b5ea1540e57fe83cdc5b0c8f419cc5e
geekymoose <constantin.masson@umontreal.ca>
2020-12-04 22:58:36 +0100

    Reintegrate Log.h and constants.h from the deprecated collab-common

---
+++ include/collabserver/utils/Log.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+
+namespace collab {
+
+
+#ifdef NDEBUG
+#define LOG std::clog << "[LOG]: "
+#else
+#define LOG std::clog << "[LOG]:[" << __func__ << "@" << __LINE__ << "]: "
+#endif
+
+
+} // End namespace
+
+
---
+++ include/collabserver/utils/constants.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define COLLAB_DEFAULT_SERVER_PORT  4242
+#define COLLAB_SOCKET_SUB_PORT      4243
+
