5b0a0c8679ac0817339dcb0b180e2b08d9f2f548
geekymoose <constantin.masson@umontreal.ca>
2020-12-04 22:44:27 +0100

    Update CMake to work with the new submodules

--- build.sh
+++ build.sh
@@ -7,12 +7,10 @@ set -v
 mkdir build || true
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-      -DCOLLAB_DEPENDENCIES_DOWNLOAD=ON \
-      -DCOLLAB_TESTS=ON \
+cmake -DCMAKE_BUILD_TYPE=${ENV_BUILD_TYPE} \
+      -DCOLLABSERVER_TESTS=ON \
       ..
 
 make
 make runAllTests
-
 
